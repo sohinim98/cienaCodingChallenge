17ee673630743faf8fb722653a8d953a37821718
Sohini Mukherjee <s23mukhe@uwaterloo.ca>
2020-06-01 16:45:47 -0400

    Updates

---
+++ precache-manifest.05f4f83f2007abf55fe82c94f1a852b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "320d1c205f062b7fbc6749cba61af79d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ade610c367958ecc1fb",
+    "url": "/static/css/main.05e11642.chunk.css"
+  },
+  {
+    "revision": "0867b3f0ca4a2d0723cf",
+    "url": "/static/js/2.7fb12a6e.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/static/js/2.7fb12a6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ade610c367958ecc1fb",
+    "url": "/static/js/main.f83cb96e.chunk.js"
+  },
+  {
+    "revision": "18e8ed90502e84bfaaf7",
+    "url": "/static/js/runtime-main.8179b21d.js"
+  }
+]);
